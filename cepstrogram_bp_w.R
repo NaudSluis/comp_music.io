6b0a5acdb12ac6eaa18f695fb1f843cc0b66fd38
Naud Sluis <naud.sluis@gmail.com>
2026-02-27 17:11:08 +0100

    hw9 send

--- cepstrogram_bp_w.R
+++ cepstrogram_bp_w.R
@@ -1,23 +1,30 @@
 library(tidyverse)
 library(compmus)
 
-bp <- read_csv("~/Documents/GitHub/comp_music/cepstrogram_bitch_please_w.csv")
+bp <- read_csv("~/Documents/GitHub/comp_music/cepstrogram_bitch_please_w_v2.csv")
 
 bp |>
   compmus_wrangle_timbre() |> 
-  mutate(timbre = map(timbre, compmus_normalise, "manhattan")) |>
+  mutate(timbre = map(timbre, compmus_normalise, "chebyshev")) |>
   compmus_gather_timbre() |>
   ggplot(
     aes(
-      x = start + duration / 2,
-      width = duration,
+      x = start,
       y = mfcc,
       fill = value
     )
   ) +
   geom_tile() +
-  labs(x = "Time (s)", y = NULL, fill = "Magnitude",
-       title = "Cepstrogram of Bitch Please by Snoop Dogg & Xzibit",
-       subtitle = "Normalization with Chebyshev method") +
-  scale_fill_viridis_c() +                              
+  labs(
+    x = "Time (s)", 
+    y = NULL, 
+    fill = "Magnitude",
+    title = "Cepstrogram of Bitch Please by Snoop Dogg & Xzibit",
+    subtitle = "Normalization with Chebyshev method"
+  ) +
+  scale_fill_viridis_c() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5)
+  ) +
   theme_classic()
